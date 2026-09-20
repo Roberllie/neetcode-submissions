9795b408ffc70acefba8c25136a4ff437bdc8b7d
Roberta Araujo <160649185+Roberllie@users.noreply.github.com>
2026-09-20 17:26:08 -0300

    Update: duplicate-integer - submission-0

--- Data Structures & Algorithms/duplicate-integer/submission-0.cpp
+++ Data Structures & Algorithms/duplicate-integer/submission-0.cpp
@@ -1,13 +1,22 @@
-#include <unordered_map>
+#include<unordered_set>
+
 class Solution {
 public:
     bool hasDuplicate(vector<int>& nums) {
-        unordered_map<int, int> mapa;
-        for(int i = 0; i < nums.size(); i++){
-            if(mapa.count(nums[i])){
+        //problema quer identificar se dado um vector de entrada
+        //ele possui numeros duplicados, se sim, retorna true, se não, retorna false
+
+        //estrutura de dados para verificar se um valor já existe: hashing 
+        //so verificar repeticões -> hash set 
+        unordered_set<int> duplicates;
+
+        for(int num : nums){
+            if(duplicates.count(num)){
                 return true;
             }
-            mapa[nums[i]] = 1;
+            else{
+                duplicates.insert(num);
+            }
         }
         return false;
     }
